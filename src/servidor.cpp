0366e5f184b5b8f9031396f8cdf0d83a78c92767
matheustomieiro <matheustomieiro@usp.br>
2020-04-14 23:56:32 -0300

    Tratando SIGINT

--- src/servidor.cpp
+++ src/servidor.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
-#include <cstdlib>
-#include <string>
-#include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h> 
 #include <netinet/in.h>
- 
+#include <unistd.h>
+
 using namespace std;
 
 //DEFININDO CONSTANTES
-#define PORTA 8080
+#define PORTA 31010
 #define TAM_FILA_REQUERENTES 3
 
+//Declarando descritor dos sockets de servidor e cliente
+int socket_servidor, socket_cliente;
 
 //Funcao que lanca uma mensagem de erro e termina o programa
 //args: (string) Mensagem de erro
@@ -19,12 +22,20 @@ void erro(string erro){
     exit(EXIT_FAILURE);
 }
 
-int main(int argc, char *argv[]){
-    int socket_servidor, socket_cliente; //Declarando descritor dos sockets de servidor e cliente
-    struct sockaddr_in socket_s_address, socket_c_address; //Declarando enderecos dos sockets(sao structs predefinidas la no socket.h)
-	char buffer[4096];
-    int tam_mensagem;
+void limpa_buffer(char *buffer){
+    for(int i=0; i<4096; i++) buffer[i] = 0;
+}
 
+void finalizar(int signum){
+    close(socket_servidor);
+    exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char *argv[]){
+    signal(SIGINT, finalizar);
+    struct sockaddr_in socket_s_address, socket_c_address; //Declarando enderecos dos sockets(sao structs predefinidas la no socket.h)
+	char buffer[4096] = {0};
+    int tam_mensagem;
 	//Criando Socket com socket()
     if((socket_servidor = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) erro("Criacao do Socket falhou!\n");
 
@@ -41,14 +52,18 @@ int main(int argc, char *argv[]){
 	
 	//Tamanho da struct
 	socklen_t tam_cliente_address = sizeof(socket_c_address);
-	
+
+    //Criando vetor de enderecos
+    struct sockaddr **enderecos = (struct sockaddr**)malloc(sizeof(struct sockaddr *)*2);
+
 	//Esperando conexao com cliente
-    while(1){
+    while(true){
 		if((socket_cliente = accept(socket_servidor, reinterpret_cast <struct sockaddr *> (&socket_c_address), &tam_cliente_address))<0) erro("Falha ao aceitar conexoes!\n");
     	if((tam_mensagem = recv(socket_cliente, buffer, 4096, 0)) < 0) erro("Falha ao receber mensagem!\n");
         printf("MENSAGEM: %s\n",buffer);
-        send(socket_cliente, buffer, strlen(buffer), 0);
+        if(buffer[0] != 0){
+            limpa_buffer(buffer);
+            printf("TAM: %d\n\n",tam_mensagem);
+        }
 	}
-	//Fim do programa
-	return EXIT_SUCCESS; 
 }
